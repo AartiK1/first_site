423e28a1b752320fcd905b0bc97998be87f83bb1
Aarti Khatri <aarti.khatri@hotmail.co.uk>
2016-07-17 21:45:29 +0100

    Addition of Back to top button
    to about page

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// FUNCTIONS TO BE RUN AFTER PAGE HAS LOADED
+$(document).ready(function() {   
+
+// BACK TO TOP BUTTON
+
+// browser window scroll (in pixels) after which the "back to top" link is shown
+var offset = 100,
+  //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+  offset_opacity = 2000, //1200px??
+  //duration of the top scrolling animation (in ms)
+  scroll_top_duration = 700,
+  //grab the "back to top" link
+  $back_to_top = $('.cd-top');
+
+//hide or show the "back to top" link
+$(window).scroll(function(){
+  ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+  if( $(this).scrollTop() > offset_opacity ) { 
+    $back_to_top.addClass('cd-fade-out');
+  }
+});
+
+//smooth scroll to top
+$back_to_top.on('click', function(event){
+  event.preventDefault();
+  $('body,html').animate({
+    scrollTop: 0 ,
+    }, scroll_top_duration
+  );
+});
+// END OF BACK TO TOP BUTTON
+        
+});
+// END OF FUNCTIONS TO BE RUN AFTER PAGE LOADS
