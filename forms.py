ffbae504053b76bd00ab8caa37c7db22c094c1a7
Aarti Khatri <aarti.khatri@hotmail.co.uk>
2016-07-21 22:30:24 +0100

    Reorganised to work with Flask
    Flask, Python & virtualenv venv

---
+++ forms.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, request
+import requests 
+
+app = Flask("MyApp")
+
+MAILGUN_DOMAIN = "sandbox69bd6b7fabef4c07875963a7013e6db4.mailgun.org"
+MAILGUN_API_KEY = "key-7afaabe46e6598ce8e19a09ff16a0b4b"
+
+def contact_us_message(to, first_name, msg):
+    return requests.post(
+        "https://api.mailgun.net/v3/%s/messages" % MAILGUN_DOMAIN, 
+        auth=("api", MAILGUN_API_KEY),
+        files = [("attachment", open("attachments/example.txt")), ("inline", open("images/happy_panda.png"))],
+        data={
+            "from": "Vegan Panda <mailgun@%s>" % MAILGUN_DOMAIN,
+            "to": to,
+            "subject": "Thank you for contacting us!",
+            "text": "Hi %s!" % first_name.split(' ').pop(0),
+            'html': '<html>Hi %s! <strong>Thank you</strong> for ... Inline image here: <img src="cid:happy_panda.png">.</html>' % first_name.split(' ').pop(0)
+        })
+        
+def contact_message(to, first_name, last_name, tel, msg):
+    return requests.post(
+        "https://api.mailgun.net/v3/lists/contact-us@%s/members" % MAILGUN_DOMAIN,
+        auth=('api', MAILGUN_API_KEY),
+        data={'subscribed': True,
+              'address': to,
+              'name': first_name + last_name,
+              'description': 'Contact Us Message',
+              'vars': '{"telephone": "%s" , "comment": "%s"}' % (tel, msg)})
+
+
+
+def subscription_message(to, first_name):
+    return requests.post(
+        "https://api.mailgun.net/v3/%s/messages" % MAILGUN_DOMAIN, 
+        auth=("api", MAILGUN_API_KEY),
+        files = [("attachment", open("example.txt")), ("inline", open("happy_panda.png"))],
+        data={
+            "from": "Vegan Panda <mailgun@%s>" % MAILGUN_DOMAIN,
+            "to": to,
+            "subject": "Welcome %s!" % first_name.split(' ').pop(0),
+            "text": "Hi %s!" % first_name.split(' ').pop(0),
+            'html': '<html> Hi %s! <strong>content</strong>. Thank you for subscribing to our newsletter... Inline image here: <img src="cid:happy_panda.png">. <br /> If you wish to unsubscribe, click </html>' % first_name.split(' ').pop(0)
+        })
+
+
+def add_list_member(to, first_name, last_name):
+    return requests.post(
+        "https://api.mailgun.net/v3/lists/newsletter@%s/members" % MAILGUN_DOMAIN,
+        auth=('api', MAILGUN_API_KEY),
+        data={'subscribed': True,
+              'address': to,
+              'name': first_name + last_name,
+              'description': 'Newsletter Subscriber'})
+             
+            
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/index.html")
+def home2():
+    return render_template("index.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+    
+@app.route("/contact/thank-you.html")
+def thank_you():
+    return render_template("thank-you.html")
+    
+@app.route("/recipes.html")
+def recipes():
+    return render_template("recipes.html")
+    
+@app.route("/ingredients.html")
+def ingredients():
+    return render_template("ingredients.html")
+    
+@app.route("/blog.html")
+def blog():
+    return render_template("blog.html")
+    
+@app.route("/terms.html")
+def terms():
+    return render_template("terms.html")
+
+@app.route("/privacy-policy.html")
+def privacy():
+    return render_template("privacy-policy.html")
+    
+@app.route("/sitemap.html")
+def sitemap():
+    return render_template("sitemap.html")
+
+
+    
+@app.route("/thank-you", methods=['GET', 'POST'])
+def sign_up():
+    form_data = request.form
+    first_name = request.form.get('fname')
+    last_name = request.form.get('lname')
+    to = request.form.get('email')
+    tel = request.form.get('tel')
+    msg = request.form.get('msg')
+    contact_us_message(to, first_name, msg)
+    contact_message(to, first_name, last_name, tel, msg)
+    subscription_message(to, first_name)
+    add_list_member(to, first_name, last_name)
+    return "All OK"
+
+
+
+
+app.run()
---
+++ static/js/main.js
@@ -0,0 +1,67 @@
+// FUNCTIONS TO BE RUN AFTER PAGE HAS LOADED
+$(document).ready(function() {   
+
+//TEXT CHARACTER COUNT
+var text_max = 500;
+$('#count_message').html(text_max + ' characters remaining');
+$('#msg').keyup(function() {
+    var text_length = $('#msg').val().length;
+    var text_remaining = text_max - text_length;
+    $('#count_message').html(text_remaining + ' characters remaining');
+});
+//END OF CHARACTER COUNT
+
+// BACK TO TOP BUTTON
+// browser window scroll (in pixels) after which the "back to top" link is shown
+var offset = 100,
+  //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+  offset_opacity = 2000, //1200px??
+  //duration of the top scrolling animation (in ms)
+  scroll_top_duration = 700,
+  //grab the "back to top" link
+  $back_to_top = $('.cd-top');
+
+//hide or show the "back to top" link
+$(window).scroll(function(){
+  ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+  if( $(this).scrollTop() > offset_opacity ) { 
+    $back_to_top.addClass('cd-fade-out');
+  }
+});
+
+//smooth scroll to top
+$back_to_top.on('click', function(event){
+  event.preventDefault();
+  $('body,html').animate({
+    scrollTop: 0 ,
+    }, scroll_top_duration
+  );
+});
+// END OF BACK TO TOP BUTTON
+        
+});
+// END OF FUNCTIONS TO BE RUN AFTER PAGE LOADS
+
+
+//FUNCTIONS TO BE RUN BEFORE PAGE HAS LOADED
+
+//Allows only numbers
+//charCode values can be found here: http://stevehardie.com/2009/09/character-code-list-char-code/ 
+//Used charCode to limit input to only numbers and + symbol
+// || means OR
+// && means AND
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if ((charCode > 31 && charCode < 43) || (charCode > 43 && (charCode < 48 || charCode > 57)))
+        return false;
+    return true;
+}
+
+//Allows only letters
+//Used charCode to limit input to only letters and hyphens
+function isAlphaKey(evt){
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if ((charCode > 32 && charCode < 45) || (charCode > 45 && charCode < 65) || (charCode > 90 && charCode < 97) || (charCode > 122))
+        return false;
+    return true;
+}
