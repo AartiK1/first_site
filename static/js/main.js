fc1deb2939fd25b0a66b8bcc3ee65cbe1ac3db90
Aarti Khatri <aarti.khatri@hotmail.co.uk>
2016-08-07 00:47:58 +0100

    map js split back into a new js file
    LHS navbar js doesnt' seem to work when maps js is in the same file

--- static/js/main.js
+++ static/js/main.js
@@ -3,72 +3,6 @@
 // FUNCTIONS TO BE RUN AFTER PAGE HAS LOADED
 $(document).ready(function() {   
 
-AmCharts.makeChart( "mapdiv_cusine", {
-  /**
-   * this tells amCharts it's a map
-   */
-  "type": "map",
-
-
-  /**
-   * create data provider object
-   * map property is usually the same as the name of the map file.
-   * getAreasFromMap indicates that amMap should read all the areas available
-   * in the map data and treat them as they are included in your data provider.
-   * in case you don't set it to true, all the areas except listed in data
-   * provider will be treated as unlisted.
-   */
-    "dataProvider": {
-    "map": "continentsLow",
-
-    "areas": [
-    { "id": "JP",
-    "url": "recipes-jap.html"},
-      { "id": "IN",
-    "url": "recipes-indian.html"},
-      { "id": "CN",
-    "url": "recipes-chinese.html"},
-    { "id": "HK",
-    "url": "recipes-chinese.html"},
-    { "id": "south_america",
-    "url": "recipes-southa.html"},
-    { "id": "north_america",
-    "url": "recipes-northa.html"},
-    { "id": "europe",
-    "url": "recipes-europe.html"},
-    { "id": "KP",
-    "url": "recipes-korean.html"},
-    { "id": "KR",
-    "url": "recipes-korean.html"},
-    { "id": "LB",
-    "url": "recipes-lebanese.html"},
-   
-  ],
-    // "getAreasFromMap": true,
-
-  },
-
-  /** 
-   * create areas settings
-   * autoZoom set to true means that the map will zoom-in when clicked on the area
-   * selectedColor indicates color of the clicked area.
-   */
-  "areasSettings": {
-    "unlistedAreasColor": "#BBBBBB",
-    "rollOverOutlineColor": "#008080",
-    "color": "rgba(129,216,208 , 1)",
-    "autoZoom": true,
-    "selectedColor": "#81D8D0",
-    "bringForwardOnHover": false,
-     
-  },
-
-  /**
-   * let's say we want a small map to be displayed, so let's create it
-  //  */
-  // "smallMap": {}
-} );
-
 /* LHS nav scrolls only to top of footer. Doesn't overlap */
 var doc = $(document); 
 doc.scroll(function () {
@@ -77,7 +11,7 @@ doc.scroll(function () {
     var p = $('#nav-fixed');
     var s = $('#left-nav-position');
 
-    var top = doc.scrollTop() + s.offset().top  * 1.95 + p.height();
+    var top = doc.scrollTop() + s.offset().top  * 2.25 + p.height();
     var footerTop = footer.offset().top;
 
     var offset = footerTop - top;
